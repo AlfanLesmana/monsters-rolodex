92d7d5c0f1ddc7e702ce970d9d5f2819acc8f9bf
Alfan Lesmana <alfanadhitia@gmail.com>
2020-07-07 15:15:24 +0700

    Updates

---
+++ precache-manifest.e1f742a1972d2ca416fee54847a4ffd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e090996e3053f953f0bf8fc0fd7c243",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "368bf82d44a2e2e279e0",
+    "url": "/monsters-rolodex/static/css/main.a4966c94.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "368bf82d44a2e2e279e0",
+    "url": "/monsters-rolodex/static/js/main.88e9dd18.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
